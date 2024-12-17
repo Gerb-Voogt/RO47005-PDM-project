01784b9b927e5f2cf0ebfbf542b359c53cf2b25d
Maarten <maarten.s.t.lam@gmail.com>
2024-12-18 00:07:38 +0100

    First working setup of closed loop MPC with nonlinear bicycle model - following a simple corner

---
+++ ACADOS/c_generated_code/CMakeFiles/ocp_pendulum.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/acados_solver_pendulum.c" "CMakeFiles/ocp_pendulum.dir/acados_solver_pendulum.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/acados_solver_pendulum.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_constraints/pendulum_constr_h_0_fun.c" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_constraints/pendulum_constr_h_0_fun_jac_uxt_zt.c" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun_jac_uxt_zt.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun_jac_uxt_zt.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_constraints/pendulum_constr_h_fun.c" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_constraints/pendulum_constr_h_fun_jac_uxt_zt.c" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun_jac_uxt_zt.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun_jac_uxt_zt.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_0_fun.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_0_fun_jac_ut_xt.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun_jac_ut_xt.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun_jac_ut_xt.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_e_fun.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_e_fun_jac_ut_xt.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun_jac_ut_xt.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun_jac_ut_xt.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_fun.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun.c.obj.d"
+  "C:/Users/maart/OneDrive/Documenten/MSc_Systems_and_Control/Year_2/Q2/PDM/Project/RO47005-PDM-project/ACADOS/c_generated_code/pendulum_cost/pendulum_cost_y_fun_jac_ut_xt.c" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun_jac_ut_xt.c.obj" "gcc" "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun_jac_ut_xt.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ACADOS/c_generated_code/CMakeFiles/ocp_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ocp_pendulum.dir/acados_solver_pendulum.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/acados_solver_pendulum.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun_jac_uxt_zt.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_0_fun_jac_uxt_zt.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun_jac_uxt_zt.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_constraints/pendulum_constr_h_fun_jac_uxt_zt.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun_jac_ut_xt.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_0_fun_jac_ut_xt.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun_jac_ut_xt.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_e_fun_jac_ut_xt.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun.c.obj.d"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun_jac_ut_xt.c.obj"
+  "CMakeFiles/ocp_pendulum.dir/pendulum_cost/pendulum_cost_y_fun_jac_ut_xt.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ocp_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
