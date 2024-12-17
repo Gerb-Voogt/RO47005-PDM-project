4ca943f0618e9cd18bfcf5d03d4d581aabc39514
Maarten <maarten.s.t.lam@gmail.com>
2024-12-18 00:07:38 +0100

    First working setup of closed loop MPC with nonlinear bicycle model - following a simple corner

---
+++ ACADOS/c_generated_code/CMakeFiles/acados_sim_solver_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "acados_sim_solver_pendulum.dll"
+  "acados_sim_solver_pendulum.dll.manifest"
+  "acados_sim_solver_pendulum.lib"
+  "acados_sim_solver_pendulum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/acados_sim_solver_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
